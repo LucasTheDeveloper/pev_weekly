c4ff4fcc7a774327344ce903e5cbd2c1d0e05b4f
LucasTheDeveloper <koketsolucas092@gmail.com>
2023-09-05 23:40:05 +0200

    copy

--- pev-weekly_automation.py
+++ pev-weekly_automation.py
@@ -17,22 +17,67 @@ def create_new_sheet(wb):
                 ws.cell(row = i, column = j).value = previous_sheet.cell(row=i , column=j).value 
     
     # Clearing the data in specified cell ranges
-    for row in ws.iter_rows(min_row=2, max_row=5, min_col=3, max_col=3):
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=3, max_col=3):
         for cell in row:
             cell.value = None
 
-    for row in ws.iter_rows(min_row=2, max_row=5, min_col=7, max_col=7):
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=6, max_col=6):
         for cell in row:
             cell.value = None
-    #copy data from a certain column and replace it on another coulumn
-    for i, row in enumerate(ws.iter_rows(min_row=2, max_row=5, min_col=1, max_col=1), start=2):
+
+
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=7, max_col=7):
+        for cell in row:
+            cell.value = None
+
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=10, max_col=10):
+        for cell in row:
+            cell.value = None
+
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=12, max_col=12):
+        for cell in row:
+            cell.value = None
+
+    
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=13, max_col=13):
+        for cell in row:
+            cell.value = None
+
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=15, max_col=15):
+        for cell in row:
+            cell.value = None
+    
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=16, max_col=16):
+        for cell in row:
+            cell.value = None
+
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=21, max_col=21):
+        for cell in row:
+            cell.value = None
+
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=24, max_col=24):
+        for cell in row:
+            cell.value = None
+    
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=26, max_col=26):
+        for cell in row:
+            cell.value = None
+
+
+    #copy data from B5 to B15 and paste it into C5 to C15
+    for i, row in enumerate(ws.iter_rows(min_row=5, max_row=15, min_col=2, max_col=2), start=5):
         ws.cell(row=i, column=3).value = row[0].value
 
-    wb.save("auto.xlsx")
+    for row in ws.iter_rows(min_row=5, max_row=15, min_col=2, max_col=2):
+        for cell in row:
+            cell.value = None
 
 
-target_time = time(11, 11)  # Set the target time to anytime
-wb = load_workbook('auto.xlsx') #loading a workbook
+    wb.save("PeV Weekly Summary Report 2023.xlsx")
+
+
+target_time = time(13,1)  # Set the target time to anytime
+wb = load_workbook('PeV Weekly Summary Report 2023.xlsx') #loading a workbook
 sheet1 = wb['sheet1']
 
 while True:
